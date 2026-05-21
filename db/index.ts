0bb21cd19935887626924f633bc7da1e96d3c073
Doberlius <tbuaekem@gmail.com>
2026-05-22 00:32:49 +0700

    feat: add supabase cilent connection

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,18 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from './schema';
 
-const client = postgres(process.env.DATABASE_URL!);
+const databaseUrl = process.env.DATABASE_URL;
 
-export const db = drizzle(client);
+if(!databaseUrl){
+    throw new Error("DATABASE_URL is not set");
+}
+
+const client = postgres(databaseUrl, {
+    prepare: false, // good for transaction pooler connection
+});
+
+export const db = drizzle(client, {schema});
+export * from "./schema";
+
+// index.ts is the file for connect the app to supabase postgres
